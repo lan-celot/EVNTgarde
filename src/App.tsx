2083525056c9cc9f272c8dd220ad167ae6c02312
Maoueee <mauicarlouisse.maramag.cics@ust.edu.ph>
2025-02-19 03:15:25 +0800

    another src update

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AccountPassword from "./Major Pages/Accounts Page/AccountPassword";
+
+const App: React.FC = () => {
+	return <AccountPassword />;
+};
+
+export default App;
